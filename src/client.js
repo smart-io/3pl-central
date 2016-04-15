b2e467834c7370a0826d8cf8d33ac82ced48b7a4
Gabriel Bull <me@gabrielbull.com>
2016-04-15 09:30:38 -0400

    Refactored close function

--- src/client.js
+++ src/client.js
@@ -2,18 +2,8 @@ import Seneca from 'seneca';
 
 let client;
 
-export function close() {
-  if (client) {
-    client.close(function (err) {
-      if (err) console.error('err: ' + err)
-    });
-  }
-}
-
 export default function () {
   if (client) return client;
-  process.stdin.resume();
-
   const seneca = Seneca({ log: 'quiet' });
 
   let config = process.env['3PL_CENTRAL_PORT'].match(/\/\/([^:]*):?(\d*)/);
@@ -22,16 +12,6 @@ export default function () {
     host: config[1],
     port: config[2]
   });
-
-  function exitHandler() {
-    if (seneca) {
-      close();
-    }
-  }
-
-  process.on('exit', exitHandler);
-  process.on('SIGINT', exitHandler);
-  process.on('uncaughtException', exitHandler);
 
   return seneca;
 };
--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-export close from './client';
+import client from './client';
+
 export findOrders from './find-orders/client';
+
+export function close() {
+  let seneca = client();
+  if (seneca) {
+    seneca.close(function (err) {
+      if (err) console.error('err: ' + err)
+    });
+  }
+}
