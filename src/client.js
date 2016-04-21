106b194da48311ba2d050973bcc212990cb970d8
Gabriel Bull <me@gabrielbull.com>
2016-04-21 12:43:20 -0400

    Started work on amqp client

--- src/client.js
+++ src/client.js
@@ -1,17 +1,61 @@
-import Seneca from 'seneca';
+import amqp from 'amqplib/callback_api';
 
-let client;
+class Client {
+  connection;
+  host;
+  eventListeners = {};
 
-export default function () {
-  if (client) return client;
-  const seneca = Seneca({ log: 'quiet' });
+  connect(host) {
+    this.host = host;
+  }
 
-  let config = process.env['3PL_CENTRAL_PORT'].match(/\/\/([^:]*):?(\d*)/);
+  close() {
+    if (this.connection) {
+      this.connection.close();
+      this.connection = null;
+    }
+  }
 
-  seneca.client({
-    host: config[1],
-    port: config[2]
-  });
+  listen() {
+    amqp.connect('amqp://' + this.host, function(err, conn) {
+      if (err) throw err;
+      this.connection = conn;
+      this.connection.createChannel(function(err, ch) {
+        const events = [
+          'new-order'
+        ];
+        for (let i = 0, len = events.length; i < len; ++i) {
+          ch.assertQueue(events[i], { durable: false });
+          ch.consume(events[i], msg => this.trigger(events[i], msg.content.toJSON()), { noAck: true });
+        }
+      });
+    });
+  }
 
-  return seneca;
-};
+  on(e, cb) {
+    if (!this.eventListeners[e]) this.eventListeners[e] = [];
+    this.eventListeners[e].push(cb);
+  }
+
+  off(e, cb) {
+    if (!cb) {
+      this.eventListeners[e] = [];
+    } else {
+      let finalEventListeners = [];
+      for (let i = 0, len = this.eventListeners[e].length; i < len; ++i) {
+        if (this.eventListeners[e][i] !== cb) finalEventListeners.push(this.eventListeners[e][i]);
+      }
+      this.eventListeners[e] = finalEventListeners;
+    }
+  }
+
+  trigger(e, data) {
+    if (this.eventListeners[e]) {
+      for (let i = 0, len = this.eventListeners[e].length; i < len; ++i) {
+        this.eventListeners[e][i](data);
+      }
+    }
+  }
+}
+
+export default Client;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
-import client from './client';
+import Client from './client';
 
 export findOrders from './find-orders/client';
-
-export function close() {
-  let seneca = client();
-  if (seneca) {
-    seneca.close(function (err) {
-      if (err) console.error('err: ' + err)
-    });
-  }
-}
+export default new Client;
