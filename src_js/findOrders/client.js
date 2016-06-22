4158c4bfd6f6aacc1712d7a5c136bf2e19ffc136
Gabriel Bull <me@gabrielbull.com>
2016-06-22 13:55:54 -0400

    Removed js code

--- src_js/findOrders/client.js
+++
@@ -1,7 +0,0 @@
-import { client } from '../index';
-
-export default function (beginDate, endDate) {
-  return new Promise((resolve, reject) => {
-    client.request('findOrders', { beginDate, endDate }).then(resolve).catch(reject);
-  });
-};
--- src_js/tasks.js
+++
@@ -1,5 +0,0 @@
-import syncOrders from './syncOrders/syncOrders';
-
-export default function () {
-  setInterval(syncOrders, 1000 * 60);
-}
